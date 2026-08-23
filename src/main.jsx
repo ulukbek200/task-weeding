c15660050ab4b2d2b3224df9cf194d7e14ce5bce
ulukbek200 <144193066+ulukbek200@users.noreply.github.com>
2026-08-23 12:58:00 +0600

    Normalize mobile responsive layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tokens.css";
 import "./styles/globals.css";
+import "./styles/mobile.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
